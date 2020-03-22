dd308c68a4a75e5ecc898370b434286ba3d6b934
Artemovych Maria <mari4ka.art.bloody@gmail.com>
2020-03-22 19:24:59 +0200

    create new form

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,19 +1,20 @@
-$('.message a').click(function () {
-    loginRegisterSwitch();
-});
+$(document).ready(function(){
+  $("div.register").hide();
+})
 
-function loginRegisterSwitch() {
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-}
+$('.message a').click(function (){
+  $("div.login").toggle();
+  $("div.register").fadeToggle("fast");
+})
 
 $("button.register")
     .click(
         function (event) {
             event.preventDefault();
-            var firstName = $("form.register-form input.firstName").val();
-            var lastName = $("form.register-form input.lastName").val();
-            var email = $("form.register-form input.email").val();
-            var password = $("form.register-form input.registerPass").val();
+            var firstName = $("form.registerForm input.userName").val();
+            var lastName = $("form.registerForm input.userSurname").val();
+            var email = $("form.registerForm input.registerEmail").val();
+            var password = $("form.registerForm input.registerPassword").val();
             if (firstName == '' || lastName == '' || email == '' || password == '') {
                 alert("Please fill all fields...!!!!!!");
             } else if ((password.length) < 4) {
@@ -46,8 +47,8 @@ $("button.login").click(function (event) {
     // need to prevent default behaviour of the button which caused page reload
     event.preventDefault();
 
-    var email = $("form.login-form input.logEmail").val();
-    var password = $("form.login-form input.loginPass").val();
+    var email = $("form.loginForm input.userEmail").val();
+    var password = $("form.loginForm input.loginPassword").val();
 
     if (email == '' || password == '') {
         alert("Please fill login form!");
@@ -59,7 +60,7 @@ $("button.login").click(function (event) {
         $.post("login", userLogin)
             .done(function (data, textStatus, xhr) {
                 if (xhr.status === 200) {
-                    window.location = "http://localhost:8888/InternetShop/cabinet.jsp";
+                     window.location = window.origin + "/InternetShop/cabinet.jsp";
                 } else {
                     alert("error while authorizing the user");
                 }
